ba6433eb14ea8cc0dd2b065657e52f0fb3750a17
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-09-21 00:01:21 +0530

    implement-upper-safe-position

--- NQueens.java
+++ NQueens.java
@@ -1,6 +1,32 @@
 public class NQueens {
 
-    public static void nQueens(char board[][], int row){
+    public static boolean isSafe(char board[][], int row, int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+
+        //diagonal left up
+        for(int i=row-1, j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        //diagonal right up
+        for(int i=row-1, j=col+1; i>=0 && j<board.length; i--,j++){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+
+    }//isSafe
+
+    public static void nQueen(char board[][], int row){
         //base case
         if(row == board.length){
             printBoard(board);
@@ -10,12 +36,13 @@ public class NQueens {
         //column loop
         for(int j=0;j<board.length;j++){
             board[row][j]='Q';
-            nQueens(board,row+1);   //function call
+            nQueen(board,row+1);   //function call
             board[row][j]='X';      //backtracking step
         }
     }//nQueens
 
     public static void printBoard(char board[][]){
+        System.out.println("---------Chess Board---------");
         for(int i=0; i<board.length;i++){
             for(int j=0; j<board.length;j++){
                 System.out.print(board[i][j]+" ");
@@ -33,7 +60,7 @@ public class NQueens {
                 board[i][j]='.';
             }
         }
-        nQueens(board,0);
+        nQueen(board,0);
     }//main
     
 }//class
