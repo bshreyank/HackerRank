bf78697c4cc7a109f8d4495623612fb8f1610cbb
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-09-20 23:49:38 +0530

    update-print

--- NQueens.java
+++ NQueens.java
@@ -15,7 +15,14 @@ public class NQueens {
         }
     }//nQueens
 
-    
+    public static void printBoard(char board[][]){
+        for(int i=0; i<board.length;i++){
+            for(int j=0; j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         int n=2;
